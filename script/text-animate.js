e9017faa49cff0138ca502e9c12bb6891ffff5cd
Eric Brian <eric.brianb12@gmail.com>
2026-04-06 18:04:35 -0300

    Uma tentativa de animar os textos, mas não está funcionando

--- script/text-animate.js
+++ script/text-animate.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const split = new SplitType(".text-reveal", {
-  types: "chars"
-});
+  const split = new SplitType(".text-reveal", {
+    types: "chars"
+  });
 
-gsap.from(split.chars, {
-  y: 40,
-  opacity: 0,
-  stagger: 0.05,
-  duration: 0.8,
-  ease: "power2.out"
-});
+  // 👇 garante que o DOM foi atualizado
+  requestAnimationFrame(() => {
+
+    gsap.from(split.chars, {
+      y: 50,
+      opacity: 0,
+      stagger: 0.1,
+      duration: 1,
+      ease: "power2.out"
+    });
+
+  });
 
 });
